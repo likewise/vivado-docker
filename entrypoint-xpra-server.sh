b79c5f77571981ded36059f6ce94ac2e3ee8cca8
Leon Woestenberg <leon@sidebranch.com>
2025-09-01 23:37:48 +0000

    Improve xpra script.

--- entrypoint-xpra-server.sh
+++ entrypoint-xpra-server.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
-XAUTHORITY= xpra start --bind-tcp=0.0.0.0:14500 --start-child-after-connect=gnome-terminal --terminate-children=yes --exit-with-children=no --daemon=yes --audio=no --webcam=no --no-tray --mdns=no --html=on --enable-pings --printing=no
-echo ""
+XAUTHORITY= xpra start --bind-tcp=0.0.0.0:14500 --start-child=gnome-terminal --start-child=vivado --terminate-children=yes --exit-with-children=no --audio=no --webcam=no --no-tray --mdns=no --html=on --enable-pings --printing=no --daemon=yes
+/bin/echo
+PUBLIC_IP=$(curl -m 2 ifconfig.me || echo "this host's")
+/bin/echo -n "Connect your (Chrome) browser to $PUBLIC_IP port 14500"
 /bin/bash
