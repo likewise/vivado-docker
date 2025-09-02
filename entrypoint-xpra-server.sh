b224bbd7bdc4316f4c1698b78fbdce75e285e8d7
Leon Woestenberg <leon@sidebranch.com>
2025-09-02 13:08:47 +0000

    entrypoint.sh: Fix missing space.

--- entrypoint-xpra-server.sh
+++ entrypoint-xpra-server.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
-XAUTHORITY= XDG_RUNTIME_DIR=/run/user/$(id -u) xpra start --bind-tcp=0.0.0.0:14500 --start-child=gnome-terminal --start-child=vivado --terminate-children=yes --exit-with-children=no --audio=no --webcam=no --no-tray --mdns=no --html=on --enable-pings --printing=no --sharing=yes--daemon=yes
+XAUTHORITY= XDG_RUNTIME_DIR=/run/user/$(id -u) xpra start \
+--bind-tcp=0.0.0.0:14500 \
+--start-child=gnome-terminal --start-child=vivado \
+--terminate-children=yes --exit-with-children=no \
+--audio=no --printing=no --webcam=no --no-tray --mdns=no \
+--enable-pings \
+--html=on \
+--sharing=yes \
+--daemon=yes
 /bin/echo
 PUBLIC_IP=$(curl -s -m 2 ifconfig.me || echo "")
 /bin/echo -n "Connect your (HTML5 compatible) browser to $PUBLIC_IP:14500\n"
