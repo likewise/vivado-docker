d6f34cd559ebfa67579403119ac41f38f9eb5417
Leon Woestenberg <leon@sidebranch.com>
2025-09-02 12:25:26 +0000

    entrypoint-xpra-server.sh: remove warning, silence curl.

--- entrypoint-xpra-server.sh
+++ entrypoint-xpra-server.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-XAUTHORITY= xpra start --bind-tcp=0.0.0.0:14500 --start-child=gnome-terminal --start-child=vivado --terminate-children=yes --exit-with-children=no --audio=no --webcam=no --no-tray --mdns=no --html=on --enable-pings --printing=no --daemon=yes
+XAUTHORITY= XDG_RUNTIME_DIR=/run/user/$(id -u) xpra start --bind-tcp=0.0.0.0:14500 --start-child=gnome-terminal --start-child=vivado --terminate-children=yes --exit-with-children=no --audio=no --webcam=no --no-tray --mdns=no --html=on --enable-pings --printing=no --sharing=yes--daemon=yes
 /bin/echo
-PUBLIC_IP=$(curl -m 2 ifconfig.me || echo "this host's")
-/bin/echo -n "Connect your (Chrome) browser to $PUBLIC_IP port 14500"
+PUBLIC_IP=$(curl -s -m 2 ifconfig.me || echo "")
+/bin/echo -n "Connect your (HTML5 compatible) browser to $PUBLIC_IP:14500\n"
 /bin/bash
